ae5529896d0e6291d40c1b82f5d5ca52780df65d
xDrakeFP <fpini3q@gmail.com>
2025-11-06 17:39:44 +0100

    Initial commit

---
+++ src/main/java/federicopini/jammee/JammeeApplication.java
@@ -0,0 +1,13 @@
+package federicopini.jammee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JammeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JammeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/federicopini/jammee/JammeeApplicationTests.java
@@ -0,0 +1,13 @@
+package federicopini.jammee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JammeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
